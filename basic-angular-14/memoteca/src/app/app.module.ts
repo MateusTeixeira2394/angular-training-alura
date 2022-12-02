2d17b2328a32ec3d445c6b6a9c3ad314e35b1e6b
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2022-12-02 14:28:54 -0300

    Added validations to create a new thought form

--- basic-angular-14/memoteca/src/app/app.module.ts
+++ basic-angular-14/memoteca/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -28,7 +28,8 @@ import { FormEditComponent } from './components/thought/form-edit/form-edit.comp
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- basic-angular-14/memoteca/src/app/components/thought/form/form.component.ts
+++ basic-angular-14/memoteca/src/app/components/thought/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ROUTE_THOUGHT_LIST } from '../../../../constants/tought/routes.constants';
 import IThought from '../../../../models/interfaces/thought.interface';
@@ -11,28 +12,43 @@ import { ThoughtService } from './../../../../services/thought.service';
 })
 export class FormComponent implements OnInit {
 
-  public thought: IThought = {
-    author: '',
-    message: '',
-    model: 'modelo1'
-  }
+  public form!: FormGroup;
 
   constructor(
     private service: ThoughtService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      message: ['', [Validators.required]],
+      author: ['', [Validators.required, Validators.minLength(3)]],
+      model: ['modelo1']
+    })
   }
 
-  public create(thought: IThought): void {
-    this.service.create(thought).subscribe(() => {
-      this.router.navigate([ROUTE_THOUGHT_LIST]);
-    });
+  public create(): void {
+    console.log(this.form)
+    if (this.form.valid) {
+      this.service.create(this.form.value).subscribe(() => {
+        this.router.navigate([ROUTE_THOUGHT_LIST]);
+      });
+    }
   };
 
   public cancel(): void {
     this.router.navigate([ROUTE_THOUGHT_LIST]);
   };
+
+  public enableButton(): string {
+
+    if (this.form.valid) {
+      return 'botao';
+    };
+
+    return 'botao__desabilitado';
+
+  }
 
 }
