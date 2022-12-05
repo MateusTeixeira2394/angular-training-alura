5bb2904a9e9b0281ea0a8a3a136044d17ada5042
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2022-12-05 07:20:51 -0300

    added validations to edit thought form

--- basic-angular-14/memoteca/src/app/components/thought/form-edit/form-edit.component.ts
+++ basic-angular-14/memoteca/src/app/components/thought/form-edit/form-edit.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ROUTE_THOUGHT_LIST } from './../../../../constants/tought/routes.constants';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ThoughtService } from './../../../../services/thought.service';
@@ -11,37 +12,52 @@ import IThought from '../../../../models/interfaces/thought.interface';
 })
 export class FormEditComponent implements OnInit {
 
-  public thought: IThought = {
-    id: 0,
-    author: '',
-    message: '',
-    model: 'modelo1'
-  }
+  public form!: FormGroup
 
   constructor(
     private service: ThoughtService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
 
     const id: string = this.route.snapshot.paramMap.get('id') || '0';
 
+    this.form = this.formBuilder.group({
+      id: [],
+      message: ['', [Validators.required]],
+      author: ['', [Validators.required, Validators.minLength(3)]],
+      model: ['modelo1']
+    });
+
     this.getById(parseInt(id));
 
   }
 
   private getById(id: number): void {
-    this.service.getById(id).subscribe(thought => this.thought = thought);
+    this.service.getById(id).subscribe(thought => {
+      this.form.setValue(thought);
+    });
   }
 
   public edit(): void {
-    this.service.edit(this.thought).subscribe(() => this.router.navigate([ROUTE_THOUGHT_LIST]));
+    this.service.edit(this.form.value).subscribe(() => this.router.navigate([ROUTE_THOUGHT_LIST]));
   }
 
   public cancel(): void {
     this.router.navigate([ROUTE_THOUGHT_LIST]);
+  }
+
+  public enableButton(): string {
+
+    if (this.form.valid) {
+      return 'botao';
+    };
+
+    return 'botao__desabilitado';
+
   }
 
 }
